17f4bea2a7de10202da9dd6ed3e078cab5f9ef04
Bonjur <55990897+MrBonjur@users.noreply.github.com>
2022-05-15 19:16:12 +0300

    Update example.py

--- random_crypt/example.py
+++ random_crypt/example.py
@@ -3,3 +3,7 @@ random.seed(302266)
 print(''.join(chr(random.randrange(256) ^ c)
     for c in bytes.fromhex('876095D97C6D7DD62EB754D1E4BFE74DD63A8C1FC2')
     if random.randrange(2)))
+
+print(''.join(chr(random.randrange(256) ^ c)
+    for c in bytes.fromhex('876095D97C6D7DD62EB754D1E4BFE74DD63A8C1FC2')
+    if random.randrange(2)))
